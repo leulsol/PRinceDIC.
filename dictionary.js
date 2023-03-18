795094bfb859e9633ace43c3ee77f04548c9bb1c
leulsol <127338157+leulsol@users.noreply.github.com>
2023-03-18 21:44:11 +0300

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,28 +1,55 @@
-const apiKey = 'd60f3946-774e-4214-bb41-2a13f5f07683';
-const searchButton = document.querySelector('#search-button');
-const searchTerm = document.querySelector('#search-term');
-const definition = document.querySelector('#definition');
+// Replace YOUR_API_KEY with your actual API key
+const apiKey = "72093e4e-2f3b-4dee-aa24-1c4f82aa3357";
 
-searchButton.addEventListener('click', () => {
-const word = searchTerm.value.trim().toLowerCase();
-  if (word !== '') {
-    fetch(`https://www.dictionaryapi.com/api/v3/references/sd4/json/${word}?key=${"d60f3946-774e-4214-bb41-2a13f5f07683"}`)
-      .then(response =>response.json())
-      .then(data => {
-        if (Array.isArray(data)) {
-          // Multiple possible definitions, so display them all
-definition.innerHTML = data.map(d => `<p>${d.shortdef.join('; ')}</p>`).join('');
-        } else if (data.shortdef) {
-          // Only one definition, so display it
-definition.innerHTML = `<p>${data.shortdef.join('; ')}</p>`;
-        } else {
-          // No definition found
-definition.innerHTML = `<p>No definition found for "${word}".</p>`;
-        }
-      })
-      .catch(error => {
-console.error(error);
-definition.innerHTML = `<p>An error occurred while retrieving the definition for "${word}".</p>`;
-      });
+const searchBtn = document.getElementById("search-btn");
+const searchInput = document.getElementById("search-input");
+const resultsContainer = document.getElementById("results-container");
+
+searchBtn.addEventListener("click", () => {
+  const word = searchInput.value.trim();
+  if (!word) {
+    alert("Please enter a word to search");
+    return;
   }
+
+  // Send GET request to API endpoint
+  fetch(`https://www.dictionaryapi.com/api/v3/references/learners/json/${word}?key=${"72093e4e-2f3b-4dee-aa24-1c4f82aa3357"}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // Clear previous results
+      resultsContainer.innerHTML = "";
+
+      // Check if word is found in the dictionary
+      if (Array.isArray(data)) {
+        // Display all definitions and example sentences
+        data.forEach((entry) => {
+          const def = entry.shortdef?.[0];
+          const fl = entry.fl;
+          const hwi = entry.hwi?.prs?.[0].mw;
+          const example = entry.uros?.[0]?.ure;
+
+          if (def) {
+            const result = document.createElement("div");
+            result.classList.add("result");
+            result.innerHTML = `
+              <h2>${word}</h2>
+              <p><em>${fl} ${hwi}</em></p>
+              <p>${def}</p>
+              ${example ? `<p>Example: ${example}</p>` : ""}
+            `;
+            resultsContainer.appendChild(result);
+          }
+        });
+      } else {
+        // Word not found
+        const result = document.createElement("div");
+        result.classList.add("result");
+        result.textContent = `No definitions found for ${word}`;
+        resultsContainer.appendChild(result);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("An error occurred while fetching data from the server. Please try again later.");
+    });
 });
